639cd98cf04498d2d9f5e4fc7d834ec184d551d3
Andrii Hrytsai <a.gritsay2@gmail.com>
2022-10-08 22:18:47 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { User } from "./User";
+import users from '../users.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <User user={users}/>
     </div>
   );
 };
